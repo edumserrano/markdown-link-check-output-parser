0076ad208bf2ce6e396daa992ba4ec51626d8723
Eduardo Serrano <eduardomserrano@outlook.com>
2022-07-03 01:09:38 +0100

    setting up test workflow  19

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -11,17 +11,17 @@ param
     $markdownFilepath
 )
 
-$output = dotnet '/app/MarkdownLinkCheckLogParserCli.dll' parse-log `
---auth-token $authToken `
---repo $repo `
---run-id $runId `
---job-name $jobName `
---step-name $stepName `
---only-errors $onlyErrors `
---output $output `
---json-filepath $jsonFilepath `
---markdown-filepath $markdownFilepath
+$command = "dotnet '/app/MarkdownLinkCheckLogParserCli.dll' parse-log --auth-token $authToken --repo $repo --run-id $runId --job-name '$jobName' --step-name '$stepName' --only-errors $onlyErrors --output $output"
+if(![string]::IsNullOrWhitespace($jsonFilepath)) 
+{
+    $command += " --json-filepath $jsonFilepath"
+}
+if(![string]::IsNullOrWhitespace($markdownFilepath))
+{
+    $command += " --markdown-filepath $markdownFilepath"
+}
 
+Invoke-Expression $command
 if($LASTEXITCODE -ne 0 ) {
     Write-Output "::error::Markdown link check log parser didn't complete successfully. See the step's log for more details."
     exit $LASTEXITCODE
