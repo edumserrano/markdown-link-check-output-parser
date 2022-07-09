10183a70ec583a6aa55ac6d4b2294180c41ecfb4
Eduardo Serrano <eduardomserrano@outlook.com>
2022-07-09 17:48:42 +0100

    update workflows 14

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,10 +1,28 @@
-Write-Output "Executing: dotnet '/app/MarkdownLinkCheckLogParserCli.dll' $args"
-$output = dotnet '/app/MarkdownLinkCheckLogParserCli.dll' $args
+function Main()
+{
+  [OutputType([Void])]
+  param ([string[]] $inputArgs)
 
-if($LASTEXITCODE -ne 0 ) {
-    Write-Output "::error::Markdown link check log parser didn't complete successfully. See the step's log for more details."
-    exit $LASTEXITCODE
+  $argsAsList = [Collections.Generic.List[String]]::new()
+  foreach ($arg in $inputArgs)
+  {
+    if(![string]::IsNullOrWhiteSpace($arg))
+    {
+      $argsAsList.Add($arg)
+    }
+  }
+  
+  Write-Output "Executing: dotnet '/app/MarkdownLinkCheckLogParserCli.dll' $argsAsList"
+  $output = dotnet '/app/MarkdownLinkCheckLogParserCli.dll' $argsAsList
+  
+  if($LASTEXITCODE -ne 0 ) {
+      Write-Output "::error::Markdown link check log parser didn't complete successfully. See the step's log for more details."
+      exit $LASTEXITCODE
+  }
+  
+  Write-Output "::set-output name=mlc-result::$output"
+  
 }
 
-Write-Output "::set-output name=mlc-result::$output"
-
+# invoke entrypoint function
+Main $args
