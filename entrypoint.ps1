b8555ddaeb7ab68ff7380afc00041b0cb614288a
Eduardo Serrano <eduardomserrano@outlook.com>
2022-07-09 15:57:19 +0100

    update entrypoint

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,34 +1,3 @@
-# param 
-# (
-#     $authToken,
-#     $repo,
-#     $runId,
-#     $jobName,
-#     $stepName,
-#     $onlyErrors,
-#     $output,
-#     $jsonFilepath,
-#     $markdownFilepath
-# )
-
-# $command = "dotnet '/app/MarkdownLinkCheckLogParserCli.dll' parse-log --auth-token '$authToken' --repo '$repo' --run-id '$runId' --job-name '$jobName' --step-name '$stepName' --only-errors $onlyErrors --output '$output'"
-# if(![string]::IsNullOrWhitespace($jsonFilepath)) 
-# {
-#     $command += " --json-filepath '$jsonFilepath'"
-# }
-# if(![string]::IsNullOrWhitespace($markdownFilepath))
-# {
-#     $command += " --markdown-filepath '$markdownFilepath'"
-# }
-
-# Write-Output "Executing: $command"
-# $output = Invoke-Expression $command
-
-
-Write-Output "==========="
-Write-Output $args
-Write-Output "==========="
-
 $output = dotnet '/app/MarkdownLinkCheckLogParserCli.dll' $args
 
 if($LASTEXITCODE -ne 0 ) {
