7808db0b2af258e19657314c79a4eb4a2ea0f7a8
Eduardo Serrano <eduardomserrano@outlook.com>
2023-01-23 20:39:34 +0000

    remove comment

--- MarkdownLinkCheckLogParser/src/MarkdownLinkCheckLogParserCli/CliCommands/ParseLog/Outputs/MarkdownConsoleOutputFormat.cs
+++ MarkdownLinkCheckLogParser/src/MarkdownLinkCheckLogParserCli/CliCommands/ParseLog/Outputs/MarkdownConsoleOutputFormat.cs
@@ -12,9 +12,6 @@ internal sealed class MarkdownConsoleOutputFormat : IOutputFormat
     public async Task WriteAsync(MarkdownLinkCheckOutput output)
     {
         output.NotNull();
-        // need to replace newline characters because this output is set as a GitHub
-        // step output and without this all newlines are lost.
-        // See https://github.community/t/set-output-truncates-multiline-strings/16852/3
         var markdownText = output.ToMarkdownText();
         await _console.Output.WriteAsync(markdownText);
     }
